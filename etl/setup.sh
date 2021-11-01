f92ca97e39d69b9253ef73abf3bc4c8b947cb955
Gustavo.Figueiredo <00747010455@RF0910100033026.rf09.srf>
2021-11-01 10:08:35 -0300

    Fixed parameters regarding Kafka discovery using Docker compose

--- etl/setup.sh
+++ etl/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # HTTP (no SSL, should only be used behind reverse proxy HTTPS->HTTP)
-java $WEB_JVM_OPTS -DLOG_DIR=/var/log -jar /home/usr/app/cacao_etl.jar --server.port=$PORT --kibana.host=$KIBANA_HOST --kibana.port=$KIBANA_PORT --kibana.endpoint=$KIBANA_ENDPOINT --es.host=$ES_HOST --es.port=$ES_PORT --mail.ssl.key-store=classpath:cert.p12 --mail.ssl.key-store-password=123456 --spring.config.additional-location=file:/app_config.properties --resource.monitor=true
+java $WEB_JVM_OPTS -DLOG_DIR=/var/log -jar /home/usr/app/cacao_etl.jar --server.port=$PORT --kibana.host=$KIBANA_HOST --kibana.port=$KIBANA_PORT --kibana.endpoint=$KIBANA_ENDPOINT --es.host=$ES_HOST --es.port=$ES_PORT --spring.cloud.stream.kafka.binder.brokers=$KAFKA_BROKERS --mail.ssl.key-store=classpath:cert.p12 --mail.ssl.key-store-password=123456 --spring.config.additional-location=file:/app_config.properties --resource.monitor=true
