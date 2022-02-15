0f72496c4774d323ea625f5bfe9523feb8b8f2e9
Rivelino Patrício <rpatricio@outlook.com.br>
2022-02-15 09:27:07 -0300

    Added shareholders an tax provision graphics. Fixed bug in customers vs suppliers reconciliation analysis.

--- web/src/main/java/org/idb/cacao/web/controllers/services/AnalysisService.java
+++ web/src/main/java/org/idb/cacao/web/controllers/services/AnalysisService.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without 
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish, 
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN 
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * This software uses third-party components, distributed accordingly to their own licenses.
+ *******************************************************************************/
 package org.idb.cacao.web.controllers.services;
 
 import java.math.BigDecimal;
@@ -52,6 +71,7 @@ import org.idb.cacao.web.dto.CustomerVsSupplier;
 import org.idb.cacao.web.dto.Outlier;
 import org.idb.cacao.web.dto.Shareholding;
 import org.idb.cacao.web.dto.StatementIncomeItem;
+import org.idb.cacao.web.utils.FormatUtils;
 import org.idb.cacao.web.utils.Script;
 import org.idb.cacao.web.utils.SearchUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -1618,7 +1638,10 @@ public class AnalysisService {
 		List<Map<String, Object>> instances = new LinkedList<>();
 
 		sresp.getHits().forEach(hit -> {
-			instances.add(hit.getSourceAsMap());
+			Map<String,Object> map = hit.getSourceAsMap();
+			Double value = (Double)map.getOrDefault("final_balance",0d);
+			map.put("finalBalanceAsString", FormatUtils.numberFormat.format(value));
+			instances.add(map);
 		});
 
 		// Get information
